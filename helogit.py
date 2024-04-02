77d2efa2d0b7c1b7a6222a8827e9557642102d85
Chache90 <118354311+Chache90@users.noreply.github.com>
2024-04-02 08:27:56 +0200

    Prueba con protocolo SSH

--- helogit.py
+++ helogit.py
@@ -1,3 +1,4 @@
 print("Helo Git, esto es una prueba, clonaciona2!")
 print("otro cambio y otro más")
-print("incluimos una nueva sentencia, hello GitHub")
+print("incluimos una nueva sentencia")
+print("Prueba con protocolo SSH")
